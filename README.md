4d8d96a5f399d1c0221569cbcd1edff4d7e5a0e4
Vitor Luiz <vitor.sesi21@gmail.com>
2024-01-06 02:29:16 -0300

    fix bugs: circlesbox 1.0.1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This application was created with the main objective of providing entertainment 
 
 ## How to use?
 
-In the foreground, Circles Box was made to WEB uses. Therefore, has HTML, CSS and JavaScript support by default on your favorite navigator. So, if you wanna play without install dependencies or source code, you can try here: <http://circleBox.vercel.app> .
+In the foreground, Circles Box was made to WEB uses. Therefore, has HTML, CSS and JavaScript support by default on your favorite navigator. So, if you wanna play without install dependencies or source code, you can try on [web site](<http://circlesbox.vercel.app>).
 
 If you wanna edit the source code or play directly in localhost mode, you should clone this repository with the following git bash command line:
 
@@ -13,3 +13,12 @@ If you wanna edit the source code or play directly in localhost mode, you should
 After running this command, CircleBox will be installed and finally you can acess the index.html in ./src/index.html or creating a self hosted server to provide a localhost acess to the file project, maybe you want to do this if you are a developer or wanna know how animations in P5.js seens.
 
 Acess index.html by a double click, or after run ```cd ./CirclesBox/src``` and ```index.html``` on cmd from windows.
+
+## Patch Notes
+
+### CirclesBox 1.0.1
+
+- New method to detect circle collisions, now only truly unknown collisions are tested.
+- Better usage of Object-oriented programming.
+- Wall collisions are now more stable, with a lower error rate.
+- Initial X, Y position doesn't match with others circles in canvas. Preventing double spawn in the same space.
--- src/sketch.js
+++ src/sketch.js
@@ -1,115 +1,13 @@
-const CPU_RADIUS = 25;           // Radius of CPU circles
-const CPU_UNITS = 5;             // Count of CPU circles on canvas
-const MAX_CPU_V = 5;             // Max CPU velocity
-const FPS = 120;                 // Frames per second of canvas
-
-// Enable Auto-play
-window.load = function() {
-  video.muted = false;
-}
+let CPU_RADIUS = 25;           // Radius of CPU circles
+let CPU_UNITS = 8;             // Count of CPU circles on canvas
+let MAX_CPU_V = 10;             // Max CPU velocity
+const FPS = 120;               // Frames per second of canvas
 
 let bounceSounds = [];
-let bouncesCount = 0;
-
 // CPU objs List
-let objList = [];
+let circleList = [];
 
-// Obj pattern
-function ObjProps() {
-  this.id = 0;
-  this.cord = {
-    x: 0,
-    y: 0,
-    vX: 0,
-    vY: 0,
-  }; this.delayedCord = {
-    vX: 0,
-    vY: 0,
-  };
-  this.rgb = {
-    r: 0,
-    g: 0,
-    b: 0,
-  };
-  this.struct = {
-    circum: 0,
-    radius: 0,
-    mass: 0,
-  };
-}
-
-// Function to Initialize CPU objs
-function initObj(obj) {
-  obj.id = objList.length + 1;
-
-  obj.struct.radius = CPU_RADIUS;
-  obj.struct.circum = CPU_RADIUS * 2;
-  obj.struct.mass = 100;
-
-  obj.cord.x = random(obj.struct.radius, windowWidth - obj.struct.radius);
-  obj.cord.y = random(obj.struct.radius, windowHeight - obj.struct.radius);
-
-  obj.cord.vX = random(MAX_CPU_V * -1, MAX_CPU_V);
-  obj.cord.vY = random(MAX_CPU_V * -1, MAX_CPU_V);
-
-  obj.rgb.r = random(0, 255);
-  obj.rgb.g = random(0, 255);
-  obj.rgb.b = random(0, 255);
-
-  objList.push(obj);
-}
-
-// Function to detect CPU colision with others CPUs
-function detectNormalObjColision(objA) {
-  for (let i = 0; i < objList.length; i++) {
-    if (objList[i] != objA) {
-      let objB = Object.create(objList[i]);
-
-      let xDif = objA.cord.x - objB.cord.x;
-      let yDif = objA.cord.y - objB.cord.y;
-      let distance = Math.sqrt(xDif * xDif + yDif * yDif);
-
-      // fill(color(0));
-      // line(objA.cord.x, objA.cord.y, objB.cord.x, objB.cord.y);
-
-      if (distance < objA.struct.radius + objB.struct.radius) {
-        objA.cord.vX = objB.delayedCord.vX;
-        objA.cord.vY = objB.delayedCord.vY;
-
-        bounceSounds[0].play();
-        bouncesCount++;
-      }
-    }
-  }
-}
-
-// Function to detect CPU colisions with border window
-function detectObjWindowColision(obj) {
-  if (
-    obj.cord.x + obj.struct.radius > windowWidth ||
-    obj.cord.x - obj.struct.radius < 0
-  ) {
-    obj.cord.vX *= -1;
-    bounceSounds[1].play();
-  }
-
-  if (
-    obj.cord.y + obj.struct.radius > windowHeight ||
-    obj.cord.y - obj.struct.radius < 0
-  ) {
-    obj.cord.vY *= -1;
-    bounceSounds[2].play();
-  }
-}
-
-// Function to move CPU objs
-function moveObj(obj) {
-  obj.cord.x += obj.cord.vX;
-  obj.cord.y += obj.cord.vY;
-  obj.delayedCord.vX = obj.cord.vX;
-  obj.delayedCord.vY = obj.cord.vY;
-}
-
+// BEFORE
 function preload() {
   soundFormats('wav');
   bounceSounds.push(loadSound('./assets/sounds/bounce1'));
@@ -117,38 +15,39 @@ function preload() {
   bounceSounds.push(loadSound('./assets/sounds/bounceWall2'));
 }
 
-// SETUP
+// ON INIT
 function setup() {
   frameRate(FPS);
-  let cnv = createCanvas(windowWidth, windowHeight);
-
+  createCanvas(windowWidth, windowHeight);
 
   // Amount of CPU Objs
   for (let n = 0; n < CPU_UNITS; n++) {
-    initObj(new ObjProps());
+    circleList.push(new Circle(windowWidth, windowHeight, CPU_RADIUS, 1));
   }
 }
 
-//DRAW
+// EACH FRAME
 function draw() {
   background(255);
 
-  // Detect CPU Objs colisions
-  for (var i = 0; i < objList.length; i++) {
-    detectObjWindowColision(objList[i], i);
-    detectNormalObjColision(objList[i], i);
+  // Detect CPU Objs collisions
+  for (var i = circleList.length - 1; i >= 0; i--) {
+    circleList[i].detectWindowcollision(windowWidth, windowHeight)
+    let hasCollision = circleList[i].detectCollision(i);
+    if (hasCollision != 0) {
+      circleList[i].handleCollision(hasCollision);
+    }
   }
 
-  for (var i = 0; i < objList.length; i++) {
-    fill(color(objList[i].rgb.r, objList[i].rgb.g, objList[i].rgb.b, 200));
-    circle(objList[i].cord.x, objList[i].cord.y, objList[i].struct.circum);
-    
-    moveObj(objList[i], i);
-
-    fill(color(0));
-    text(objList[i].id, objList[i].cord.x, objList[i].cord.y);
+  // Draw CPU Objs
+  for (var i = circleList.length - 1; i >= 0; i--) {
+    circleList[i].move();
+    circleList[i].display();
+    fill(0);
+    text(i, circleList[i].cord.x, circleList[i].cord.y);
   }
 
+  // Mouse Circle
   fill(255);
   circle(mouseX, mouseY, CPU_RADIUS);
 }
